313eaa7afb0f443f153ba441ee4fbc692255441e
土田駿介 <nemu.shun@icloud.com>
2025-11-24 10:48:54 +0900

    [REFACT] add comment to index.ts

--- docker/api/nodejs/src/index.ts
+++ docker/api/nodejs/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import mysql from "mysql2/promise";
 
+// Express.js: 使用するための魔法
 const app = express();
+
+// API側のポート番号を書いておく
 const port = 3000;
 
 // DB 接続関数
@@ -14,14 +17,14 @@ async function getConnection() {
   });
 }
 
-// /users → DB のデータを返す
+// Express.js: GETリクエストで /users → DB のデータを返す
 app.get("/users", async (_req, res) => {
   const conn = await getConnection();
   const [rows] = await conn.execute("SELECT * FROM users");
   res.json(rows);
 });
 
-// サーバー起動
+// Express.js: 3000番ポートでサーバー起動
 app.listen(port, () => {
   console.log(`API server running on port ${port}`);
 });
